3c6b2dcd87b5cc3b470b73f2adfe248b91a5f73b
sideeq12 <abdwaheed2018@gmail.com>
2020-12-19 18:44:22 -0800

    setting

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const { Server } = require("http");
 const https = require("https");
 
 let Url ="https://sv443.net/jokeapi/v2/joke/Programming,Pun,Spooky,Christmas?blacklistFlags=religious,racist,sexist"
+const TwiterUrl = "https://api.twitter.com/1.1/statuses/update.json"
 https.get(Url, (Response)=>{
     Response.on("data", (data)=>{
         let Joke = JSON.parse(data)
@@ -13,4 +14,5 @@ https.get(Url, (Response)=>{
         }
     })
 })
+
 
