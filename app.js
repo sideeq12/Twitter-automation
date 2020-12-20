e8cedab2ec955a11da4bc5294d833cde628cf121
sideeq12 <abdwaheed2018@gmail.com>
2020-12-19 18:38:42 -0800

    api fetch

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+const { Server } = require("http");
 const https = require("https");
 
 let Url ="https://sv443.net/jokeapi/v2/joke/Programming,Pun,Spooky,Christmas?blacklistFlags=religious,racist,sexist"
 https.get(Url, (Response)=>{
     Response.on("data", (data)=>{
-        console.log(JSON.parse(data))
+        let Joke = JSON.parse(data)
+        let setup = Joke.setup;
+        if(setup){
+            console.log(setup  + " - " + Joke.delivery) 
+        }else{
+            console.log(Joke.joke)
+        }
     })
 })
+
