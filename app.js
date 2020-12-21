60900355162af66b3c0e5216b1bbfe22cc478d13
sideeq12 <abdwaheed2018@gmail.com>
2020-12-21 02:56:39 -0800

    post

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const { Server } = require("http");
 const https = require("https");
 const unirest = require("unirest");
 
+// unirest
+//   .post('http://mockbin.com/request')
+//   .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+//   .send({ "parameter": 23, "foo": "bar" })
+//   .then((response) => {
+//     console.log(response.body)
+//   })
+
 let Url ="https://sv443.net/jokeapi/v2/joke/Programming,Pun,Spooky,Christmas?blacklistFlags=religious,racist,sexist"
 const TwiterUrl = "https://api.twitter.com/1.1/statuses/update.json"
 https.get(Url, (Response)=>{
