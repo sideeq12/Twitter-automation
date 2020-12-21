1017cc17640f8bc04c06bded5553e08a108bf4e9
sideeq12 <abdwaheed2018@gmail.com>
2020-12-21 04:56:25 -0800

    trial

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 const { Server } = require("http");
 const https = require("https");
-const twitter = require("twitter-lite")
+const Twitter = require("twitter-lite")
 require("dotenv").config();
-const file= require("./config")
-console.log(file)
 
 
 
-let Url ="https://sv443.net/jokeapi/v2/joke/Programming,Pun,Spooky,Christmas?blacklistFlags=religious,racist,sexist"
-const TwiterUrl = "https://api.twitter.com/1.1/statuses/update.json"
-https.get(Url, (Response)=>{
-    Response.on("data", (data)=>{
-        let Joke = JSON.parse(data)
-        let setup = Joke.setup;
-        if(setup){
-            console.log(setup  + " - " + Joke.delivery) 
-        }else{
-            console.log(Joke.joke)
-        }
-    })
-})
+var client = new Twitter({
+    consumer_key: process.env.API,
+    consumer_secret: process.env.Secret,
+    bearer_token: process.env.Bearer
+  });
+  client.post('statuses/update', {status: 'I Love Twitter'})
+  .then(function (tweet) {
+    console.log(tweet);
+  })
+  .catch(function (error) {
+    throw error;
+  })
+
+
+
+// let Url ="https://sv443.net/jokeapi/v2/joke/Programming,Pun,Spooky,Christmas?blacklistFlags=religious,racist,sexist"
+// const TwiterUrl = "https://api.twitter.com/1.1/statuses/update.json"
+// https.get(Url, (Response)=>{
+//     Response.on("data", (data)=>{
+//         let Joke = JSON.parse(data)
+//         let setup = Joke.setup;
+//         if(setup){
+//             console.log(setup  + " - " + Joke.delivery) 
+//         }else{
+//             console.log(Joke.joke)
+//         }
+//     })
+// })
 
 
--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    consumer_key : process.env.API,
-    consumer_secret : process.env.Secret,
-    bearer_token : process.env.bearer
-}
