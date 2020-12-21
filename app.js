2516c8e6d89b0607281a880d95a352c64806db8c
sideeq12 <abdwaheed2018@gmail.com>
2020-12-21 04:21:20 -0800

    twitter node

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const { Server } = require("http");
 const https = require("https");
 const unirest = require("unirest");
 
-// unirest
-//   .post('http://mockbin.com/request')
-//   .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
-//   .send({ "parameter": 23, "foo": "bar" })
-//   .then((response) => {
-//     console.log(response.body)
-//   })
+unirest
+  .post('http://mockbin.com/request')
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .send({ "parameter": 23, "foo": "bar" })
+  .then((response) => {
+    console.log(response.body)
+  })
 
 let Url ="https://sv443.net/jokeapi/v2/joke/Programming,Pun,Spooky,Christmas?blacklistFlags=religious,racist,sexist"
 const TwiterUrl = "https://api.twitter.com/1.1/statuses/update.json"
