c221779e08f84fa2eea9b33ea8ea187f20f2f37e
sideeq12 <abdwaheed2018@gmail.com>
2020-12-22 22:13:39 -0800

    st

--- app.js
+++ app.js
@@ -18,18 +18,18 @@ var client = new Twitter({
 
 
 
-// let Url ="https://sv443.net/jokeapi/v2/joke/Programming,Pun,Spooky,Christmas?blacklistFlags=religious,racist,sexist"
-// const TwiterUrl = "https://api.twitter.com/1.1/statuses/update.json"
-// https.get(Url, (Response)=>{
-//     Response.on("data", (data)=>{
-//         let Joke = JSON.parse(data)
-//         let setup = Joke.setup;
-//         if(setup){
-//             console.log(setup  + " - " + Joke.delivery) 
-//         }else{
-//             console.log(Joke.joke)
-//         }
-//     })
-// })
+let Url ="https://sv443.net/jokeapi/v2/joke/Programming,Pun,Spooky,Christmas?blacklistFlags=religious,racist,sexist"
+const TwiterUrl = "https://api.twitter.com/1.1/statuses/update.json"
+https.get(Url, (Response)=>{
+    Response.on("data", (data)=>{
+        let Joke = JSON.parse(data)
+        let setup = Joke.setup;
+        if(setup){
+            console.log(setup  + " - " + Joke.delivery) 
+        }else{
+            console.log(Joke.joke)
+        }
+    })
+})
 
 
